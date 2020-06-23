246432cf8611cc34492c2b730e06fcbd43e6c259
Justin Villamor <Jv.Villamor132@gmail.com>
2020-06-23 02:13:48 -0400

    Adding Compass image references and added a class for controller related settings

---
+++ Assets/Scripts/Player/Compass.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller
+{
+    public enum Dir
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        UpLeft,//NorthWest
+        UpRight,//NorthEast
+        DownLeft,//SouthWest
+        DownRight,//SouthEast
+        Middle//Doing nothing/ joystick idle
+    }
+
+    public Dir direction;
+    public float setDead = 0.5f; //Deadzone for when to decide to set the enum
+    public float setOffsetDead;//Deadzone offset for diagnols
+}
+
+public class Compass : MonoBehaviour
+{    
+    public Image north;
+    public Image south;
+    public Image east;
+    public Image west;
+    public Image northeast;
+    public Image northwest;
+    public Image southeast;
+    public Image southwest;
+    public Image middle;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
